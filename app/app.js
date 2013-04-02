b61acbf49a987eaee65d50b1d1241ecf902814f9
Emily Myers <emilyhannahmyers@gmail.com>
2013-04-02 19:04:30 -0400

    finish tutorial, prep for new content

---
+++ app/app.js
@@ -0,0 +1,40 @@
+var express = require("express"),
+    http = require("http"),
+    path = require("path"),
+    redisClient = require("redis").createClient(),
+    app = express();
+
+app.configure(function () {                                                                                           
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+http.createServer(app).listen(3000, function(){
+    console.log("Express server listening on port 3000");
+});
+
+app.get("/", function (req, res) {
+    //send "Hello World" to the client as html
+    res.send("Hello World!");
+});
+
+app.get("/goodbye", function (req, res) {
+    //send "Goodbye World" to the client as html
+    res.send("Goodbye World!");
+});
+
+app.get("/login", function (req, res) {
+    res.send("You need to login!");
+});
+
+app.get("/counts.json", function (req, res) {
+    redisClient.get("awesome", function (error, awesomeCount) {
+    if (error !== null) {
+            // handle error here                                                                                                                       
+            console.log("ERROR: " + error);
+        }
+        else {
+            var jsonObject = {"awesome":awesomeCount};
+            res.json(jsonObject);
+        }
+    });
+});
--- app/twitter.js
+++ app/twitter.js
@@ -1,5 +1,10 @@
 var twitter = require('ntwitter');
+var redis = require('redis');
 var credentials = require('./credentials.js');
+var http = require("http");
+
+//create redis client                                                                                                                                                                                              
+var client = redis.createClient();
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
@@ -14,6 +19,31 @@ t.stream(
     function(stream) {
         stream.on('data', function(tweet) {
             console.log(tweet.text);
+            //if awesome is in the tweet text, increment the counter
+            if(tweet.text.indexOf("awesome") > -1) {
+                client.incr('awesome');
+            }
         });
     }
 );
+
+var server = http.createServer(function (req, res) {
+    client.mget(["awesome","cool"], function(err, results) {
+        if (err !== null) {
+            //handle error here
+            console.log("error: " + err);
+        } else {
+            var response += "<script>";
+            response += "//script stuff here";
+            response += "</script>";
+            response += "<style>";
+            response += "//style stuff here";
+            response += "</style>";
+            response += "<b>Hello from my http server!!</b> <br/>";
+        response += "<p>Total awesome: " + results[0] + "</p>";
+        response += "<p>Total cool: " + results[1] + "</p>";
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(response);
+        }
+    });
+}).listen(3000);
